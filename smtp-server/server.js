35f1114be9b35bdce451b88971d2ef23dffa579a
Prince <prince2520p@gmail.com>
2024-10-23 19:46:15 +0530

    refactor: mail data

--- smtp-server/server.js
+++ smtp-server/server.js
@@ -6,30 +6,47 @@ require("dotenv").config();
 const server = new SMTPSERVER({
     allowInsecureAuth: true,
     authOptional: true,
-    
+
     onData(stream, session, cb) {
         stream.on('data', (data) => {
             simpleParser(data, async (err, parsed) => {
-                
+
                 console.log("Parsed email data: ", parsed);
                 console.log("Server Url", process.env.SERVERURL);
+
+                const data = {
+                    headers: parsed.headers,
+                    subject: parsed.subject,
+                    from: parsed.from,
+                    to: parsed.to,
+                    cc: parsed.cc,
+                    bcc: parsed.bcc,
+                    date: parsed.date,
+                    messageId: parsed.messageId,
+                    inReplyTo: parsed.inReplyTo,
+                    references: parsed.references,
+                    html: parsed.html,
+                    text: parsed.text,
+                    textAsHtml: parsed.textAsHtml,
+                    attachments: parsed.attachments
+                }
 
                 try {
                     await fetch(
                         `${process.env.SERVERURL}/mail/get-mail`,
                         {
-                          method: "POST",
-                          headers: {
-                            "Content-Type": "application/json",
-                          },
-                          body: JSON.stringify(parsed),
+                            method: "POST",
+                            headers: {
+                                "Content-Type": "application/json",
+                            },
+                            body: JSON.stringify(data),
                         }
                     );
 
-                }catch(err){
+                } catch (err) {
                     console.log(err);
                 }
-                
+
             });
         });
         stream.on('end', cb);
