2f4d7f068f0d3104e9d1207f60b1b44cc8300a57
Prince <prince2520p@gmail.com>
2025-03-30 11:28:27 +0530

    refactor: vite config file

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,6 +4,10 @@ import { defineConfig } from "vite"
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: process.env.PORT ? parseInt(process.env.PORT) : 5173, // Default to 5173 if PORT is not set
+    host: '0.0.0.0' // Allow external access (important for Render)
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
